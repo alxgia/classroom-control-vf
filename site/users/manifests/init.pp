b5a05cee8b4968327d219b41fc21605bc16ab02d
alxgia <agiacari@gmail.com>
2016-06-29 11:17:26 -0400

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,3 +1,8 @@
-user { 'fundamentals':
-  ensure => present,
+class users { 
+users {'fundamentals':
+      ensure => present,
+  #   password => 'puppet8#labs', # Windows requires a plain text password
+  #   groups   => ['Users'],      # Display in Windows Control Panel
   }
+}
+
