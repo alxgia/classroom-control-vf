2442e81e8eec171d3a4a56e32055ee6d1b8e5391
alxgia <agiacari@gmail.com>
2016-06-30 13:06:24 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,12 +1,5 @@
 class users::admins { 
 users::managed_user { 'alex': } 
-users::managed_user { 'king':
-group => 'staff', 
-}
-users::managed_user { 'jay': 
-group => 'staff',
-}
-group { 'staff':
-ensure => present, 
-  }
+users::managed_user { 'king': }
+
 }
