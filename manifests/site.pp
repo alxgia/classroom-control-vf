e446e410a45c82f5b3733f19abe735f3534011e0
alxgia <agiacari@gmail.com>
2016-06-30 09:11:19 -0400

    fix code

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,14 +55,14 @@ node default {
     
     $cowsay_cmd = "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd"
     $cowsay_unless = '/bin/grep -F "Welcome to" /etc/motd'
-  
+  }
     exec { 'cowsay_motd':
     command => $cowsay_cmd,
     unless  => $cowsay_unless,
       }
     }
     
-      }
+    
    
 
 
