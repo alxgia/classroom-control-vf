6ed4c24cb4039b30e85205973eaf23713ded4680
alxgia <agiacari@gmail.com>
2016-06-29 20:27:17 -0400

    update init.pp 2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,12 @@
-class nginx { package { 'nginx':
-ensure => present, }
-file { '/var/www': ensure => directory, owner => 'root', group => 'root', mode => '0775',
+class nginx {
+ package { 'nginx':
+  ensure => present,
+}
+file { '/var/www': 
+  ensure => directory,
+  owner => 'root',
+  group => 'root', 
+  mode => '0775',
 }
 file { '/var/www/index.html':
 ensure => file,
@@ -10,12 +16,32 @@ mode => '0664',
 source => 'puppet:///modules/nginx/index.html',
 }
 file { '/etc/nginx/nginx.conf':
-ensure => file,
-  owner
-group
-mode
-source => 'puppet:///modules/nginx/nginx.conf', require => Package['nginx'],
-=> 'root', => 'root',
-=> '0664',
-notify => Service['nginx'],
-}
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode  => '0664',
+  source => 'puppet:///modules/nginx/nginx.conf', 
+  require => Package['nginx'],
+  notify => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+   ensure => directory, 
+   owner => 'root', 
+   group => 'root',
+   mode => '0775',
+   }
+   file { '/etc/nginx/conf.d/default.conf':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode  => '0664',
+  source => 'puppet:///modules/nginx/default.conf', 
+  require => Package['nginx'],
+  notify => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running
+    enable => true,
+      }
+    )
+   
