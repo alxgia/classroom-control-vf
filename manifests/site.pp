11e9f27235893761e3f3079b61bc0ca0203b38c1
alxgia <agiacari@gmail.com>
2016-06-29 21:52:39 -0400

    typo

--- manifests/site.pp
+++ manifests/site.pp
@@ -59,13 +59,14 @@ node default {
     exec { 'cowsay_motd':
     command => $cowsay_cmd,
     unless  => $cowsay_unless,
+    }
     
     host { 'testing.puppetlabs.vm':
      ensure => present,
      ip => '127.0.0.1',
        }
       }
-   }
+   
 
 
 
